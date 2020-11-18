c28413bad361b76bed8deb5655dc4e98fe5eef11
Hunter Larco <hunter@larcolabs.com>
2020-11-18 18:38:17 -0500

    `errors` field should not create a TwitterError (#27)

--- examples/search_tweets.js
+++ examples/search_tweets.js
@@ -6,21 +6,29 @@ async function main() {
   const client = new Twitter(await credentials.fromCommandLine());
 
   // Recent Tweet Search API Reference: https://bit.ly/3jqFjKF
-  const { data: tweets, meta } = await client.get('tweets/search/recent', {
-    query: 'url:"https://medium.com" -is:retweet lang:en',
-    max_results: 10,
-    tweet: {
-      fields: [
-        'created_at',
-        'entities',
-        'in_reply_to_user_id',
-        'public_metrics',
-        'referenced_tweets',
-        'source',
-        'author_id',
-      ],
-    },
-  });
+  const { data: tweets, meta, errors } = await client.get(
+    'tweets/search/recent',
+    {
+      query: 'url:"https://medium.com" -is:retweet lang:en',
+      max_results: 10,
+      tweet: {
+        fields: [
+          'created_at',
+          'entities',
+          'in_reply_to_user_id',
+          'public_metrics',
+          'referenced_tweets',
+          'source',
+          'author_id',
+        ],
+      },
+    }
+  );
+
+  if (errors) {
+    console.log('Errors:', errors);
+    return;
+  }
 
   for (const tweet of tweets) {
     console.log(tweet);

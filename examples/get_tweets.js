1fbc7d3a45ebac84e691831445e1932834ec92dc
hunterlarco <hunter@larcolabs.com>
2020-11-18 18:36:19 -0500

    `errors` field should not create a TwitterError

--- examples/get_tweets.js
+++ examples/get_tweets.js
@@ -6,14 +6,19 @@ async function main() {
   const client = new Twitter(await credentials.fromCommandLine());
 
   // Tweet Lookup API Reference: https://bit.ly/2QF58Kw
-  const { data: tweet } = await client.get('tweets', {
+  const { data: tweet, errors } = await client.get('tweets', {
     ids: '1228393702244134912',
     tweet: {
       fields: ['created_at', 'entities', 'public_metrics', 'author_id'],
     },
   });
 
-  console.log(tweet);
+  if (errors) {
+    console.log('Errors:', errors);
+    return;
+  }
+
+  console.log('Tweet:', tweet);
 }
 
 if (require.main === module) {

eed842a8622b3fbd01b1ad151ba6d1a3eb0caa11
Brandon Scott <brandongregoryscott@gmail.com>
2020-11-18 20:12:03 -0500

    Second pass at setting up types, interfaces & classes for current lib implementation

--- src/twitter.d.ts
+++ src/twitter.d.ts
@@ -1,27 +1,64 @@
-declare module "twitter-v2" {
-    export interface Credentials {
-        consumer_key?: string;
-        consumer_secret?: string;
-        bearer_token?: string;
-        access_token_key?: string;
-        access_token_secret?: string;
-    }
+declare module 'twitter-v2' {
+  export interface ApplicationCredentials {
+    consumer_key: string;
+    consumer_secret: string;
+    bearer_token?: string;
+  }
 
-    export interface TwitterResponse<T extends any> {
-        data: T;
-    }
+  export interface UserCredentials {
+    consumer_key: string;
+    consumer_secret: string;
+    access_token_key: string;
+    access_token_secret: string;
+  }
 
-    export default class Twitter {
-        constructor(credentials: Credentials);
-        public get<T>(
-            endpoint: string,
-            parameters?: any
-        ): Promise<TwitterResponse<T>>;
-        public post<T>(
-            endpoint: string,
-            body?: any,
-            parameters?: any
-        ): Promise<TwitterResponse<T>>;
-        public stream(endpoint: string, parameters?: any): any;
-    }
+  export type CredentialsArgs = ApplicationCredentials | UserCredentials;
+
+  export interface RequestParameters {
+    [key: string]: any;
+  }
+
+  export interface TwitterResponse<T extends any> {
+    data: T;
+  }
+
+  export class Credentials implements ApplicationCredentials, UserCredentials {
+    constructor(args: CredentialsArgs);
+    public consumer_key: string;
+    public consumer_secret: string;
+    public access_token_key: string;
+    public access_token_secret: string;
+    public bearer_token?: string;
+    public appAuth(): boolean;
+    public userAuth(): boolean;
+    public createBearerToken(): Promise<string>;
+    public authorizationHeader(url: string): Promise<string>;
+  }
+
+  export class TwitterStream<T> implements AsyncIterator<TwitterResponse<T>> {
+    constructor(connect: () => Promise<any>, close: () => void);
+    public close(): void;
+    next(
+      ...args: [] | [undefined]
+    ): Promise<IteratorResult<TwitterResponse<T>>>;
+    return?(value?: any): Promise<IteratorResult<TwitterResponse<T>, any>>;
+    throw?(e?: any): Promise<IteratorResult<TwitterResponse<T>, any>>;
+  }
+
+  export default class Twitter {
+    constructor(credentials: CredentialsArgs);
+    public get<T>(
+      endpoint: string,
+      parameters?: RequestParameters
+    ): Promise<TwitterResponse<T>>;
+    public post<T>(
+      endpoint: string,
+      body?: any,
+      parameters?: RequestParameters
+    ): Promise<TwitterResponse<T>>;
+    public stream<T>(
+      endpoint: string,
+      parameters?: RequestParameters
+    ): TwitterStream<T>;
+  }
 }

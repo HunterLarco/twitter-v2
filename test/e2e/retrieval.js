95cd2ca7031f8b0323fff1206d651b93b467c66e
hunterlarco <hunter@larcolabs.com>
2021-02-25 18:40:25 -0500

    e2e tests

---
+++ test/e2e/retrieval.js
@@ -0,0 +1,50 @@
+const { expect } = require('chai');
+
+const Twitter = require('../../src/twitter.js');
+
+if (!process.env.TWITTER_DISABLE_E2E) {
+  describe('e2e retrieval', () => {
+    it('should retrieve tweets with bearer token', async () => {
+      const client = new Twitter({
+        bearer_token: process.env.TWITTER_BEARER_TOKEN,
+      });
+
+      // Tweet Lookup API Reference: https://bit.ly/2QF58Kw
+      const { data: tweet, errors } = await client.get('tweets', {
+        ids: '1228393702244134912',
+        tweet: {
+          fields: ['created_at', 'entities', 'public_metrics', 'author_id'],
+        },
+      });
+
+      expect(errors).to.be.undefined;
+      expect(tweet[0]).to.include({
+        author_id: '2244994945',
+        created_at: '2020-02-14T19:00:55.000Z',
+        id: '1228393702244134912',
+      });
+    });
+
+    it('should retrieve tweets with consumer tokens', async () => {
+      const client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      });
+
+      // Tweet Lookup API Reference: https://bit.ly/2QF58Kw
+      const { data: tweet, errors } = await client.get('tweets', {
+        ids: '1228393702244134912',
+        tweet: {
+          fields: ['created_at', 'entities', 'public_metrics', 'author_id'],
+        },
+      });
+
+      expect(errors).to.be.undefined;
+      expect(tweet[0]).to.include({
+        author_id: '2244994945',
+        created_at: '2020-02-14T19:00:55.000Z',
+        id: '1228393702244134912',
+      });
+    });
+  });
+}
---
+++ test/e2e/streaming.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+
+const Twitter = require('../../src/twitter.js');
+
+if (!process.env.TWITTER_DISABLE_E2E) {
+  describe('e2e search', () => {
+    it('should stream data without any rules', async () => {
+      const client = new Twitter({
+        bearer_token: process.env.TWITTER_BEARER_TOKEN,
+      });
+
+      // Sampled Stream API Reference: https://bit.ly/31CU870
+      const stream = client.stream('tweets/sample/stream');
+
+      // Read data from the stream
+      for await (const { data } of stream) {
+        expect(data.id).to.be.an('string');
+        stream.close();
+      }
+    });
+  });
+}

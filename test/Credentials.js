b53817ac2952a9bcd81461a76b094366e283f205
hunterlarco <hunter@larcolabs.com>
2021-02-25 17:26:20 -0500

    accept just a bearer token

--- test/Credentials.js
+++ test/Credentials.js
@@ -3,17 +3,39 @@ const { expect } = require('chai');
 const Credentials = require('../src/Credentials.js');
 
 describe('Credentials', () => {
-  it('should require a consumer key and secret', () => {
+  it('should enforce that credentials are defined', () => {
     expect(() => new Credentials({})).to.throw(
-      'Missing required argument: consumer_key'
+      'Invalid argument: no credentials defined'
     );
+  });
 
-    expect(() => new Credentials({ consumer_key: 'foo' })).to.throw(
-      'Missing required argument: consumer_secret'
+  it('should enforce consumer key and secret parity', () => {
+    expect(
+      () =>
+        new Credentials({
+          consumer_key: 'foo',
+        })
+    ).to.throw(
+      'Invalid argument: when using consumer keys, both consumer_key and ' +
+        'consumer_secret must be defined'
     );
 
     expect(
-      () => new Credentials({ consumer_key: 'foo', consumer_secret: 'bar' })
+      () =>
+        new Credentials({
+          consumer_secret: 'bar',
+        })
+    ).to.throw(
+      'Invalid argument: when using consumer keys, both consumer_key and ' +
+        'consumer_secret must be defined'
+    );
+
+    expect(
+      () =>
+        new Credentials({
+          consumer_key: 'foo',
+          consumer_secret: 'bar',
+        })
     ).to.not.throw();
   });
 
@@ -64,15 +86,47 @@ describe('Credentials', () => {
           access_token_secret: 'quz',
         })
     ).to.throw(
-      'Invalid argument: access_token_key and access_token_secret should not ' +
-        'be used with bearer_token'
+      'Invalid argument: access_token_key and access_token_secret cannot be ' +
+        'used with bearer_token'
     );
+  });
+
+  it('should accept either consumer keys or bearer token for app auth', () => {
+    expect(
+      () =>
+        new Credentials({
+          consumer_key: 'foo',
+          consumer_secret: 'bar',
+        })
+    ).to.not.throw();
+
+    expect(
+      () =>
+        new Credentials({
+          bearer_token: 'baz',
+        })
+    ).to.not.throw();
+
+    expect(
+      () =>
+        new Credentials({
+          consumer_key: 'foo',
+          consumer_secret: 'bar',
+          bearer_token: 'baz',
+        })
+    ).to.not.throw();
   });
 
   it('should recognize app vs user auth', () => {
     let credentials = new Credentials({
       consumer_key: 'foo',
       consumer_secret: 'bar',
+    });
+    expect(credentials.appAuth()).to.be.true;
+    expect(credentials.userAuth()).to.be.false;
+
+    credentials = new Credentials({
+      bearer_token: 'foo',
     });
     expect(credentials.appAuth()).to.be.true;
     expect(credentials.userAuth()).to.be.false;

2e1540b7376731aeb70ab9c0b763778b1e80029a
Hunter Larco <hunter@larcolabs.com>
2021-02-25 18:59:22 -0500

    e2e tests (#51)

---
+++ test/e2e/index.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+
+if (process.env.TWITTER_DISABLE_E2E) {
+  console.warn(chalk.yellow('e2e tests are disabled'));
+} else if (
+  !process.env.TWITTER_BEARER_TOKEN ||
+  !process.env.TWITTER_CONSUMER_KEY ||
+  !process.env.TWITTER_CONSUMER_SECRET
+) {
+  console.error(chalk.red('e2e tests require Twitter credentials.'));
+  console.log();
+  console.log('To provide credentials, define the environment variables:');
+  console.log(chalk.dim('- TWITTER_CONSUMER_KEY'));
+  console.log(chalk.dim('- TWITTER_CONSUMER_SECRET'));
+  console.log(chalk.dim('- TWITTER_BEARER_TOKEN'));
+  console.log();
+  console.log('or, skip e2e tests with TWITTER_DISABLE_E2E');
+  console.log();
+  process.exit(1);
+}
---
+++ test/e2e/search.js
@@ -0,0 +1,72 @@
+const { expect } = require('chai');
+
+const Twitter = require('../../src/twitter.js');
+
+if (!process.env.TWITTER_DISABLE_E2E) {
+  describe('e2e search', () => {
+    it('should search tweets with bearer token', async () => {
+      const client = new Twitter({
+        bearer_token: process.env.TWITTER_BEARER_TOKEN,
+      });
+
+      // Recent Tweet Search API Reference: https://bit.ly/3jqFjKF
+      const { data: tweets, meta, errors } = await client.get(
+        'tweets/search/recent',
+        {
+          query: 'url:"https://medium.com" -is:retweet lang:en',
+          max_results: 10,
+          tweet: {
+            fields: [
+              'created_at',
+              'entities',
+              'in_reply_to_user_id',
+              'public_metrics',
+              'referenced_tweets',
+              'source',
+              'author_id',
+            ],
+          },
+        }
+      );
+
+      expect(errors).to.be.undefined;
+      expect(tweets).to.not.be.empty;
+      expect(meta).to.include({
+        result_count: tweets.length,
+      });
+    });
+
+    it('should search tweets with consumer tokens', async () => {
+      const client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      });
+
+      // Recent Tweet Search API Reference: https://bit.ly/3jqFjKF
+      const { data: tweets, meta, errors } = await client.get(
+        'tweets/search/recent',
+        {
+          query: 'url:"https://medium.com" -is:retweet lang:en',
+          max_results: 10,
+          tweet: {
+            fields: [
+              'created_at',
+              'entities',
+              'in_reply_to_user_id',
+              'public_metrics',
+              'referenced_tweets',
+              'source',
+              'author_id',
+            ],
+          },
+        }
+      );
+
+      expect(errors).to.be.undefined;
+      expect(tweets).to.not.be.empty;
+      expect(meta).to.include({
+        result_count: tweets.length,
+      });
+    });
+  });
+}

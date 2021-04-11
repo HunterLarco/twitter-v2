67dbdaeada8fabf0d205fec34b68c61add812a4d
hunterlarco <hunter@larcolabs.com>
2021-04-11 14:25:40 -0400

    filtered example

--- examples/stream_tweets.js
+++ examples/stream_tweets.js
@@ -2,21 +2,56 @@ const Twitter = require('../build/twitter.js');
 
 const credentials = require('./helpers/credentials.js');
 
+async function resetRules(client, desiredRule) {
+  const existingRules = await client.get('tweets/search/stream/rules');
+
+  if (existingRules.data) {
+    const rulesToDelete = existingRules.data.filter(
+      (rule) => rule.value !== desiredRule
+    );
+    if (rulesToDelete.length) {
+      console.log(
+        'Deleting rules:',
+        rulesToDelete.map((rule) => rule.value)
+      );
+      await client.post('tweets/search/stream/rules', {
+        delete: {
+          ids: rulesToDelete.map((rule) => rule.id),
+        },
+      });
+    }
+  }
+
+  if (
+    !existingRules.data ||
+    !existingRules.data.some((rule) => rule.value === desiredRule)
+  ) {
+    console.log('Creating rule:', desiredRule);
+    await client.post('tweets/search/stream/rules', {
+      add: [
+        {
+          value: desiredRule,
+          tag: Math.round(Math.random() * 10e16).toString(),
+        },
+      ],
+    });
+  }
+}
+
 async function main() {
   const client = new Twitter(await credentials.fromCommandLine());
 
-  // Sampled Stream API Reference: https://bit.ly/31CU870
-  const stream = client.stream('tweets/sample/stream');
+  await resetRules(client, 'from:SpdpTest');
+  const stream = client.stream('tweets/search/stream');
 
-  // Close the stream after 30 seconds
-  setTimeout(() => {
-    stream.close();
-  }, 30 * 1000);
+  console.log('start', new Date());
 
   // Read data from the stream
   for await (const { data } of stream) {
     console.log(`${data.id}: ${data.text.replace(/\s/g, ' ')}`);
   }
+
+  console.log('end', new Date());
 
   console.log('Stream closed.');
 }
